2ef8a161a3e0ea21a88d2a9867382272e97f56e2
gavrielF-mobi1 <30917565+gavrielF@users.noreply.github.com>
2018-04-20 12:58:46 +0300

    monitor test

--- ver2/src/com/start/LineFollwer.java
+++ ver2/src/com/start/LineFollwer.java
@@ -1,10 +1,36 @@
 package com.start;
 
+import lejos.nxt.Button;
+import lejos.robotics.RegulatedMotor;
+import lejos.util.PilotProps;
+
 public class LineFollwer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception
+	{
+		
+		PilotProps pp = new PilotProps();
+    	pp.loadPersistentValues();
+    	float wheelDiameter = Float.parseFloat(pp.getProperty(PilotProps.KEY_WHEELDIAMETER, "4.96"));
+    	float trackWidth = Float.parseFloat(pp.getProperty(PilotProps.KEY_TRACKWIDTH, "13.0"));
+    	RegulatedMotor leftMotor = PilotProps.getMotor(pp.getProperty(PilotProps.KEY_LEFTMOTOR, "B"));
+    	RegulatedMotor rightMotor = PilotProps.getMotor(pp.getProperty(PilotProps.KEY_RIGHTMOTOR, "C"));
 
+    	
+		leftMotor.setSpeed(900);
+		leftMotor.forward();
+		while(!Button.ENTER.isDown())
+		{
+					
+		}
+		leftMotor.stop();
+		rightMotor.setSpeed(900);
+		rightMotor.forward();
+		while(!Button.ENTER.isDown())
+		{
+					
+		}
+		rightMotor.stop();	
 	}
 
 }
