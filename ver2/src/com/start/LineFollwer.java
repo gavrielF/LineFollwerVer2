37c19f7889ba411c5b1bcde836910cfc0c276f63
gavrielF <30917565+gavrielF@users.noreply.github.com>
2018-04-22 23:22:04 +0300

    add logger class

--- ver2/src/com/start/LineFollwer.java
+++ ver2/src/com/start/LineFollwer.java
@@ -1,5 +1,7 @@
 package com.start;
 
+import com.logger.*;
+
 import lejos.nxt.Button;
 import lejos.robotics.RegulatedMotor;
 import lejos.util.PilotProps;
@@ -17,20 +19,42 @@ public class LineFollwer {
     	RegulatedMotor rightMotor = PilotProps.getMotor(pp.getProperty(PilotProps.KEY_RIGHTMOTOR, "C"));
 
     	
-		leftMotor.setSpeed(900);
-		leftMotor.forward();
-		while(!Button.ENTER.isDown())
-		{
-					
-		}
-		leftMotor.stop();
-		rightMotor.setSpeed(900);
-		rightMotor.forward();
-		while(!Button.ENTER.isDown())
-		{
-					
-		}
-		rightMotor.stop();	
+//		leftMotor.setSpeed(900);
+//		leftMotor.forward();
+//		while(!Button.ENTER.isDown())
+//		{
+//					
+//		}
+//		leftMotor.stop();
+//		rightMotor.setSpeed(900);
+//		rightMotor.forward();
+//		while(!Button.ENTER.isDown())
+//		{
+//					
+//		}
+//		rightMotor.stop();	
+    	
+    	
+
+
+		
+		
+    	Logger.getInstance().logDebug("ffffffffffff");
+    	Logger.getInstance().logDebug("ggg4");
+    	Logger.getInstance().logDebug("ggg5");
+    	Logger.getInstance().logDebug("ggg6");
+
+    	Logger.getInstance().logDebug("ggg7");
+    	Logger.getInstance().logDebug("gfggs" , 34);
+    	Logger.getInstance().logDebug("gfgfgfggfgfg" , 34);
+    	Logger.getInstance().logDebug("gfggs" , 34);
+    	Logger.getInstance().logDebug("gfggs" , 34);
+    	Logger.getInstance().logDebug("44444444444444444" , 34);
+    	Logger.getInstance().logDebug("fffffffffffffffffffffffffffff3442");
+    	Logger.getInstance().logDebug("fffffffffffffffffffffdsfdf");
+    	Logger.getInstance().logDebug("fgrs" , 4563111);
+    	
+    	Logger.getInstance().write();
 	}
 
 }
